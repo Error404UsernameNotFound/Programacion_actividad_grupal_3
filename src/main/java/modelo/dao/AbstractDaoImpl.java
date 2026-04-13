b858d0b89089072f7498c3a43bbba8f9dc7670b7
luciarmas <luacoarmas@gmail.com>
2026-04-13 13:53:41 +0100

    Código actualizado

---
+++ src/main/java/modelo/dao/AbstractDaoImpl.java
@@ -0,0 +1,23 @@
+package modelo.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
+public abstract class AbstractDaoImpl {
+
+    protected EntityManagerFactory emf;
+    protected EntityManager em;
+    protected EntityTransaction tx;
+    protected Query query;
+    protected String jpql;
+    protected int filas;
+
+    public AbstractDaoImpl() {
+        emf = Persistence.createEntityManagerFactory("proyectosFP");
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
+    }
+}
